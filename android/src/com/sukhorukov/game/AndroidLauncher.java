66edf3a448ca993188a98e02a5523fe804c0bcd4
Soohoy <soohoy@bk.ru>
2018-02-24 13:15:21 +0500

    Initial project

---
+++ android/src/com/sukhorukov/game/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.sukhorukov.game;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.sukhorukov.game.AgainstClouds;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new AgainstClouds(), config);
+	}
+}
---
+++ desktop/src/com/sukhorukov/game/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.sukhorukov.game.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.sukhorukov.game.AgainstClouds;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new AgainstClouds(), config);
+	}
+}
